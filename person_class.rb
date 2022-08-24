0da28a283c27255dd2185bc56d8fe5cd9efd01f2
maen <maenkh@yahoo.com>
2022-08-24 22:21:26 +0300

    finish the nameable and decorator classes

--- person_class.rb
+++ person_class.rb
@@ -1,12 +1,20 @@
+require './nameable_class'
+
+
 # This is the main class of the application
-class Person
+class Person < Nameable
   attr_accessor :name, :age, :parent_permission
 
-  def initialize(age, name = 'Unknown', parent_permission: true)
+  def initialize(age, name = 'Unknown', parent_permission: true)    
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
+    super()
+  end
+
+  def correct_name
+    @name
   end
 
   def of_age?
@@ -17,3 +25,12 @@ class Person
     @age > 18 || @parent_permission == true
   end
 end
+
+
+# person = Person.new(22, 'maximilianus')
+# person.correct_name
+# capitalizedPerson = CapitalizeDecorator.new(person)
+# capitalizedPerson.correct_name
+
+# capitalizedTrimmedPerson = TrimmerDecorator.new(capitalizedPerson)
+#   capitalizedTrimmedPerson.correct_name
