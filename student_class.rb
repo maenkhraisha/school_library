1d6003937ddd75ce5b834ab254416d8f7dd7822a
maen <maenkh@yahoo.com>
2022-08-25 10:52:35 +0300

    add classroom class

--- student_class.rb
+++ student_class.rb
@@ -2,10 +2,16 @@ require './Person_Class'
 
 # This is the teacher class inherit from person
 class Student < Person
+  attr_reader :classroom
   def initialize(age, classroom, name, parent_permission)
     super(name, parent_permission, age)
     @classroom = classroom
   end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
+  end 
 
   def play_hooky
     "¯\(ツ)/¯"
