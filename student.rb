9ab92dd219cc232da0af4c48d51ad7d6e0b8dbd3
maen <maenkh@yahoo.com>
2022-08-25 17:56:49 +0300

    fix rubocop

--- student.rb
+++ student.rb
@@ -2,10 +2,10 @@ require './person'
 
 # This is the teacher class inherit from person
 class Student < Person
-  attr_reader :classroom, :parent_permission
+  attr_reader :classroom
 
   def initialize(classroom, age, name = 'unknown', parent_permission: true)
-    super(age, name)
+    super(age, name, parent_permission)
     @classroom = classroom
   end
 
