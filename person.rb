d4eba24875f9d85a97dbf7b4da5ec0f825a12c8b
maen <maenkh@yahoo.com>
2022-08-30 14:02:45 +0300

    fix linter

--- person.rb
+++ person.rb
@@ -5,7 +5,7 @@ class Person < Nameable
   attr_reader :id, :parent_permission, :rentals
   attr_accessor :name, :age
 
-  def initialize(age, name , parent_permission)
+  def initialize(age, name, parent_permission)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
