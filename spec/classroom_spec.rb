e02ad36317d493318b083587d67888cfc2f6d18c
maen <maenkh@yahoo.com>
2022-09-01 14:33:40 +0300

    fix linter

--- spec/classroom_spec.rb
+++ spec/classroom_spec.rb
@@ -1,10 +1,10 @@
 require './classroom_class'
 
-describe Classroom do 
-    context "giveen parameter" do
-        it "should have name" do
-            class_one = Classroom.new("class A")
-            expect(class_one.label).to eql "class A"
-        end
+describe Classroom do
+  context 'giveen parameter' do
+    it 'should have name' do
+      class_one = Classroom.new('class A')
+      expect(class_one.label).to eql 'class A'
     end
+  end
 end
